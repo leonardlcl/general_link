b2482776076094ce7bc41173767ef9d4d0f7246e
guxiaoqi2007 <243757555@qq.com>
2024-07-23 21:33:21 +0800

    用异步后台来更新mqtt连接

--- custom_components/general_link/__init__.py
+++ custom_components/general_link/__init__.py
@@ -1,55 +1,31 @@
-"""The Detailed MHTZN integration."""
-from __future__ import annotations
-
 import logging
-import threading
 import time
-
+import asyncio
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_NAME
 from homeassistant.core import HomeAssistant
-
+from homeassistant.const import CONF_NAME
 from .Gateway import Gateway
 from .const import PLATFORMS, MQTT_CLIENT_INSTANCE, CONF_LIGHT_DEVICE_TYPE, DOMAIN, FLAG_IS_INITIALIZED, \
     CACHE_ENTITY_STATE_UPDATE_KEY_DICT, CONF_BROKER
 from .mdns import MdnsScanner
 
 _LOGGER = logging.getLogger(__name__)
-
-monitor_exec_flag = True
-
-global_thread_id = 1
-
+reconnect_flag = asyncio.Event()
 
 async def _async_config_entry_updated(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """This method is triggered when the entry configuration changes, and the gateway connection is updated"""
     hub = hass.data[DOMAIN][entry.entry_id]
-
-    """reconnect gateway"""
-    # await hub.reconnect(entry)
-    """Initialize gateway information and synchronize child device list to HA"""
     hass.async_create_task(
         hub.init(entry, False)
     )
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    global monitor_exec_flag, global_thread_id
     """Set up from a config entry."""
     hub = Gateway(hass, entry)
-
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = hub
 
-    """Set a flag to record whether the current integration has been initialized"""
-    if FLAG_IS_INITIALIZED not in hass.data:
-        hass.data[FLAG_IS_INITIALIZED] = False
+    hass.data.setdefault(FLAG_IS_INITIALIZED, False)
+    hass.data.setdefault(CACHE_ENTITY_STATE_UPDATE_KEY_DICT, {})
 
-    """Set a dictionary to record whether the sub-device state change event has been created, 
-    to avoid the same device from repeatedly creating state change events"""
-    if CACHE_ENTITY_STATE_UPDATE_KEY_DICT not in hass.data:
-        hass.data[CACHE_ENTITY_STATE_UPDATE_KEY_DICT] = {}
-
-    """Determine whether the current integration has been initialized to avoid repeated installation of platform list"""
     if not hass.data[FLAG_IS_INITIALIZED]:
         hass.data[FLAG_IS_INITIALIZED] = True
         await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
@@ -58,120 +34,88 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hub.reconnect_flag = True
 
-    """Initialize gateway information and synchronize child device list to HA"""
     hass.async_create_task(
         hub.init(entry, True)
     )
 
-    monitor_exec_flag = True
+    reconnect_flag.clear()
 
-    def update_config_entry(entry: ConfigEntry, data_dict: dict):
-        hass.add_job(_update_config_entry,entry,data_dict)
+    #reconnect_flag = asyncio.Event()
     
-    
-    async def _update_config_entry(entry: ConfigEntry, new_data: dict) -> None:
-        await hass.config_entries.async_update_entry(entry, data=new_data)
-
-    def entry_reload(entry: ConfigEntry) -> None:
-        _LOGGER.warning('reload GE integration...')
-        hass.add_job(hass.config_entries.async_reload,entry.entry_id)
-
-    def monitor_connection():
-        scanner = MdnsScanner()
-
-
-
-        time.sleep(30)
-
-        # 获取当前时间戳
-        current_time = time.time()
-
-        # 设置初始时间戳
-        start_time = current_time
-
-        # 定义间隔时间（10分钟）
-        interval = 5 * 60
-
-        thread_id = global_thread_id
-
-        while monitor_exec_flag and thread_id == global_thread_id:
-            try:
-                #_LOGGER.warning("线程ID %s 全局ID %s", thread_id, global_thread_id)
-                entry_data = entry.data
-                # status = connect_mqtt(entry_data[CONF_BROKER], entry_data[CONF_PORT]
-                #                      , entry_data[CONF_USERNAME], entry_data[CONF_PASSWORD])
-                #_LOGGER.warning("status：%s，hub.init_state：%s", entry.data, hub.init_state)
-                mqtt_connected = hub.hass.data[MQTT_CLIENT_INSTANCE].connected
-                #_LOGGER.warning("mqtt  %s", mqtt_connected)
-                if not mqtt_connected or not hub.init_state:
-                    hub.reconnect_flag = True
-                    connection = scanner.scan_single(entry_data[CONF_NAME], 5)
-                    _LOGGER.warning("mqtt 连接不上了，需要重新扫描一下，得到连接 %s", connection)
-                    if connection is not None:
-                        if CONF_LIGHT_DEVICE_TYPE in entry_data:
-                            connection[CONF_LIGHT_DEVICE_TYPE] = entry_data[CONF_LIGHT_DEVICE_TYPE]
-                            connection["random"] = time.time()
-                        update_config_entry(
-                            entry,
-                            connection,
-                            )
-                elif mqtt_connected and hub.init_state:
-                    current_time = time.time()
-                    if current_time - start_time >= interval:
-                        # 执行你的操作
-                        hass.create_task(
-                            hub.sync_group_status(False)
-                        )
-                        # 更新起始时间戳
-                        start_time = current_time
-                time.sleep(10)
-            except OSError as err:
-                _LOGGER.error("进程ERROR: %s", err)
-
-    monitor_thread = threading.Thread(target=monitor_connection)
-    monitor_thread.start()
-
-    """Add an entry configuration change event listener to trigger the specified method 
-    when the configuration changes"""
     entry.add_update_listener(_async_config_entry_updated)
 
-    # entry.async_on_unload(entry.add_update_listener(update_listener))
 
+    await asyncio.sleep(2)
+
+    hass.async_create_background_task(
+
+        monitor_connection(hass, hub, entry, reconnect_flag),
+
+        "monitor_connection"
+
+    )
+    
     return True
 
+async def monitor_connection(hass, hub, entry, reconnect_flag):
 
-def connect_mqtt(broker: str, port: int, username: str, password: str):
-    from paho.mqtt import client
-    try:
-        client = client.Client("test-connect")
-        client.username_pw_set(username, password=password)
-        client.connect(broker, port)
-        client.disconnect()
-        return True
-    except OSError as err:
-        _LOGGER.error("Failed to connect to MQTT server due to exception: %s", err)
-        return False
+    scanner = MdnsScanner(hass)
+
+    last_sync_time = 0  # 用于记录上一次同步的时间
+
+
+    while not reconnect_flag.is_set():
+
+        try:
+
+            mqtt_connected = hub.hass.data[MQTT_CLIENT_INSTANCE].connected
+
+            current_time = time.time()
+
+            if not mqtt_connected or not hub.init_state:
+
+                hub.reconnect_flag = True
+
+                connection = await scanner.scan_single(entry.data[CONF_NAME], 10)
+
+                _LOGGER.warning("connection bk  %s", connection)
+
+                if connection is not {}:
+
+                    if CONF_LIGHT_DEVICE_TYPE in entry.data:
+
+                        connection[CONF_LIGHT_DEVICE_TYPE] = entry.data[CONF_LIGHT_DEVICE_TYPE]
+
+                        connection["random"] = time.time()
+
+                await hass.config_entries.async_update_entry(entry, data=connection)
+
+
+            elif current_time - last_sync_time >= 300 :
+                
+                last_sync_time = current_time
+
+                await hub.sync_group_status(False)
+
+
+        except Exception as e:
+
+            _LOGGER.error("Error in monitor_connection: %s", e)
+
+
+        await asyncio.sleep(20)  # 每20秒检测一次
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    global monitor_exec_flag, global_thread_id
-    """This method is triggered when the entry is unload"""
+
+    reconnect_flag.set()  # Notify monitor_connection to stop
 
     await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     hub = hass.data[DOMAIN].pop(entry.entry_id)
-    """Perform a gateway disconnect operation"""
+
     await hub.disconnect()
 
     hass.data[CACHE_ENTITY_STATE_UPDATE_KEY_DICT] = {}
 
-    monitor_exec_flag = False
-
-    global_thread_id = global_thread_id + 1
-
     return True
-
-
-# async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
-#     """Handle options update."""
-#     await hass.config_entries.async_reload(config_entry.entry_id)
