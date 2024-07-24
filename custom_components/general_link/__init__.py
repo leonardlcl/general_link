883ab6fadb7e96b2232226d0da4743ef401e2d56
guxiaoqi2007 <243757555@qq.com>
2024-07-24 13:31:36 +0800

    更新注释修复问题

--- custom_components/general_link/__init__.py
+++ custom_components/general_link/__init__.py
@@ -13,25 +13,40 @@ _LOGGER = logging.getLogger(__name__)
 reconnect_flag = asyncio.Event()
 
 async def _async_config_entry_updated(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """当配置项更新时的异步处理函数。
+    参数:
+    - hass: HomeAssistant对象，表示Home Assistant实例。
+    - entry: ConfigEntry对象，表示配置项。
+    """
     hub = hass.data[DOMAIN][entry.entry_id]
     hass.async_create_task(
         hub.init(entry, False)
     )
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """基于配置项的异步设置函数。
+    参数:
+    - hass: HomeAssistant对象，表示Home Assistant实例。
+    - entry: ConfigEntry对象，表示配置项。
+    返回:
+    - bool: 表示设置是否成功。
+    """
     """Set up from a config entry."""
     hub = Gateway(hass, entry)
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = hub
 
+    # 初始化标记和实体状态更新键字典
     hass.data.setdefault(FLAG_IS_INITIALIZED, False)
     hass.data.setdefault(CACHE_ENTITY_STATE_UPDATE_KEY_DICT, {})
 
+    # 如果尚未初始化，则进行初始化操作
     if not hass.data[FLAG_IS_INITIALIZED]:
         hass.data[FLAG_IS_INITIALIZED] = True
         await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     else:
         await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
+    # 启用重连标志
     hub.reconnect_flag = True
 
     hass.async_create_task(
@@ -42,6 +57,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     #reconnect_flag = asyncio.Event()
     
+    # 注册配置项更新监听器
     entry.add_update_listener(_async_config_entry_updated)
 
 
@@ -58,7 +74,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 async def monitor_connection(hass, hub, entry, reconnect_flag):
-
+    """监控连接的异步函数。
+    参数:
+    - hass: HomeAssistant对象，表示Home Assistant实例。
+    - hub: Gateway对象，表示网关。
+    - entry: ConfigEntry对象，表示配置项。
+    - reconnect_flag: asyncio.Event对象，用于控制重连逻辑。
+    """
     scanner = MdnsScanner(hass)
 
     last_sync_time = 0  # 用于记录上一次同步的时间
@@ -68,18 +90,22 @@ async def monitor_connection(hass, hub, entry, reconnect_flag):
 
         try:
 
+            # 检查MQTT连接状态
             mqtt_connected = hub.hass.data[MQTT_CLIENT_INSTANCE].connected
 
             current_time = time.time()
 
+            # 如果MQTT未连接或网关初始化状态为False，则尝试重新连接
             if not mqtt_connected or not hub.init_state:
 
                 hub.reconnect_flag = True
 
+                # 通过mDNS扫描设备
                 connection = await scanner.scan_single(entry.data[CONF_NAME], 10)
 
-                _LOGGER.warning("connection bk  %s", connection)
+                _LOGGER.warning("mqtt 连接不上了，需要重新扫描一下，得到连接 %s", connection)
 
+                # 如果扫描到设备，更新配置项数据
                 if connection is not {}:
 
                     if CONF_LIGHT_DEVICE_TYPE in entry.data:
@@ -88,9 +114,12 @@ async def monitor_connection(hass, hub, entry, reconnect_flag):
 
                         connection["random"] = time.time()
 
-                await hass.config_entries.async_update_entry(entry, data=connection)
+                        _LOGGER.warning("connection rk  %s", connection)
+
+                    await hass.config_entries.async_update_entry(entry, data=connection)
 
 
+            # 每300秒同步一次群组状态
             elif current_time - last_sync_time >= 300 :
                 
                 last_sync_time = current_time
@@ -103,11 +132,16 @@ async def monitor_connection(hass, hub, entry, reconnect_flag):
             _LOGGER.error("Error in monitor_connection: %s", e)
 
 
-        await asyncio.sleep(20)  # 每20秒检测一次
-
+        await asyncio.sleep(20)  # 每20秒检测一次连接状态
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-
+    """卸载配置项的异步函数。
+    参数:
+    - hass: HomeAssistant对象，表示Home Assistant实例。
+    - entry: ConfigEntry对象，表示配置项。
+    返回:
+    - bool: 表示卸载是否成功。
+    """
     reconnect_flag.set()  # Notify monitor_connection to stop
 
     await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
